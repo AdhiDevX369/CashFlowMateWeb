1bca71053dbb4d63bff1fd6f556f6e67c7328a69
Adithya Bandara <dammikaekanayaka1980@gmail.com>
2023-06-29 00:23:39 +0530

    imgs links

--- Dashboard/Js/constants.js
+++ Dashboard/Js/constants.js
@@ -1,18 +1,18 @@
 const UPDATE_DATA = [
     {
-      imgSrc: "/assests/img/user/user3.jpg",
+      imgSrc: "../assests/img/user/user3.jpg",
       profileName: "Mike Tyson",
       message: "updated expenses of building. ",
       updatedTime: "3 Days Ago",
     },
     {
-      imgSrc: "/assests/img/user/user2.jpg",
+      imgSrc: "../assests/img/user/user2.jpg",
       profileName: "Damian Anul",
       message: "Updated Expenses of Payment.",
       updatedTime: "5 Days Ago",
     },
     {
-      imgSrc: "/assests/img/user/client.jpg",
+      imgSrc: "../assests/img/user/client.jpg",
       profileName: "Srenali Maria",
       message: "Add Expenses of Entertainment.",
       updatedTime: "6 Days Ago",
